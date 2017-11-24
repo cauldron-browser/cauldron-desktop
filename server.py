3f4e7d90690e18ce097561948424cddf5c22756b
Jason Seibel <jasonLseibel@gmail.com>
2017-11-25 00:36:34 +0200

    wget multithreaded and has limit on max num of threads - using a thread pool or threading library might have been better / would have made this prettier

--- server.py
+++ server.py
@@ -1,15 +1,27 @@
 import subprocess
 import sys
+import time
 from flask import Flask, request
+global listOfSubs
+listOfSubs = []
 app = Flask(__name__)
 
 @app.route("/visit", methods=['POST'])
 def visit():
+    
     if request.method == 'POST':
         url = request.form['url']
         print(url)
-        subprocess.Popen(wget_command(url), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        return "Post Recieved!"
+        #OnExit is callback to limit number of subprocesses to 2
+        while (len(listOfSubs) > 3):
+            time.sleep(0.3)
+            #remove any process that finished
+            for sb in listOfSubs:
+                if sb.poll() is 0:
+                    listOfSubs.remove(sb)
+        listOfSubs.append(subprocess.Popen(wget_command(url), stdout=subprocess.PIPE, stderr=subprocess.STDOUT))
+        return "Post Received!"
+
 
 @app.route("/search")
 def search():
@@ -24,3 +36,6 @@ def wget_command(url):
     Return the parsed command for the wget command of a given url.
     """
     return "wget -r -N --no-remove-listing --convert-links --adjust-extension --page-requisites --no-parent {}".format(url).split()
+
+if __name__ == '__main__':
+    app.run(threaded=True)
