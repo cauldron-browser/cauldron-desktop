ddfb9242b41fe85e8fdf6371e75e5326a77fdb0c
Ajay Jain <ajayjain@mit.edu>
2017-11-25 08:29:11 -0500

    Create a query_parser on each search, due to possible concurrency bug

--- index.py
+++ index.py
@@ -9,11 +9,12 @@ INDEX_DIR = os.environ.get("INDEX_DIR", "index")
 
 class Index(object):
     def __init__(self):
-        # Initialize schema and index
-        schema = whoosh.fields.Schema(title=whoosh.fields.TEXT(stored=True), 
-                                      url=whoosh.fields.ID(stored=True), 
+        # Initialize schema for index creation
+        schema = whoosh.fields.Schema(title=whoosh.fields.TEXT(stored=True),
+                                      url=whoosh.fields.ID(stored=True),
                                       body_text=whoosh.fields.TEXT)
 
+        # Create index and index object. self.index can be shared between threads.
         if not os.path.exists(INDEX_DIR):
             print("Creating search index at {}".format(INDEX_DIR))
             os.mkdir(INDEX_DIR)
@@ -21,9 +22,6 @@ class Index(object):
         else:
             print("Loading search index at {}".format(INDEX_DIR))
             self.index = whoosh.index.open_dir(INDEX_DIR)
-
-        # Create a query_parser that searches the body text by default
-        self.query_parser = whoosh.qparser.QueryParser("body_text", self.index.schema)
 
     def index_html(self, html_file_path):
         # Parse contents of HTML file
@@ -39,8 +37,13 @@ class Index(object):
         writer.commit()
 
     def search(self, query_string):
-        query = self.query_parser.parse(query_string)
+        # Parse user query string
+        query_parser = whoosh.qparser.QueryParser("body_text", self.index.schema)
+        query = query_parser.parse(query_string)
+
+        # Search for results in the index
         searcher = self.index.searcher()
         results = searcher.search(query)
+
         return results
 
