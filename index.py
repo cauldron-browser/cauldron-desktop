7b3d1fd0045a7d53da9f5b4e85fee40033c102ff
Ajay Jain <ajayjain@mit.edu>
2017-11-25 08:18:27 -0500

    Make index writing thread safe

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import os
 import whoosh.index
 import whoosh.fields
 import whoosh.qparser
+import whoosh.writing
 
 INDEX_DIR = os.environ.get("INDEX_DIR", "index")
 
@@ -12,8 +13,8 @@ query_parser = None
 
 def init():
     # Initialize schema and index
-    schema = whoosh.fields.Schema(title=whoosh.fields.TEXT(stored=True), 
-                                  url=whoosh.fields.ID(stored=True), 
+    schema = whoosh.fields.Schema(title=whoosh.fields.TEXT(stored=True),
+                                  url=whoosh.fields.ID(stored=True),
                                   body_text=whoosh.fields.TEXT)
 
     if not os.path.exists(INDEX_DIR):
@@ -27,15 +28,23 @@ def init():
     # Create a query_parser that searches the body text by default
     query_parser = whoosh.qparser.QueryParser("body_text", index.schema)
 
-def add_to_index(title, url, body_text):
+def index_html(html_file_path):
+    # Parse contents of HTML file
+    # TODO(ajayjain): Deduplicate with Luis's code?
+
+    # Add to the index
+    index_parsed(title, url, body_text)
+
+def index_parsed(title, url, body_text):
     # TODO(ajayjain): Bulk write documents to the index
-    writer = index.writer()
+    writer = whoosh.writing.AsyncWriter(index)
     writer.add_document(title=title, url=url, body_text=body_text)
     writer.commit()
 
 def search(query_string):
     query = query_parser.parse(query_string)
-    results = index.searcher().search(query)
+    searcher = index.searcher()
+    results = searcher.search(query)
     return results
 
 init()
