f8662d0464cf25a6e60d7ef0941e67bd2d8b447a
Luis Torres <luillo@mit.edu>
2017-11-25 19:59:08 -0500

    Time optimization

--- algLogic.py
+++ algLogic.py
@@ -7,10 +7,12 @@ from rake_nltk import Rake
 import index
 from google import search
 import random
+import numpy as np
+import time
 
 def findAllLinks(url):
     # Gets all the links that are present on a webpage
-    
+
     resp = urllib.request.urlopen(url)
     soup = BeautifulSoup(resp, 'lxml')
 
@@ -55,14 +57,23 @@ def extractKeywords(text):
     r.extract_keywords_from_text(text)
     return r.get_ranked_phrases() # To get keyword phrases ranked highest to lowest.
 
-def contentSimilarity(soupPage1,soupPage2):
+def contentSimilarity(url1,url2):
     #TODO: given two pages/soups/sets of keywords, evaluates how similar/mutually relevant their content
+    resp1 = urllib.request.urlopen(url1)
+    soup1 = BeautifulSoup(resp1, 'lxml')
+    page1 = index.parse_html_string(str(soup1))
 
-    website_documents = [meta1, meta2]
+    resp2 = urllib.request.urlopen(url2)
+    soup2 = BeautifulSoup(resp2, 'lxml')
+    page2 = index.parse_html_string(str(soup2))
+
+    website_documents = [page1.content, page2.content]
     website_documents_split = [x.strip().split() for x in website_documents]
-    m = gensim.models.Doc2Vec.load("word2vec.bin")
-    website_vecs = [m.infer_vector(d, alpha=0.01, steps=1000) for d in website_documents_split]
 
+    website_vecs = [m.infer_vector(d, alpha=0.01, steps=1000) for d in website_documents_split]
+    vec1 = website_vecs[0]
+    vec2 = website_vecs[1]
+    #print(np.dot(vec1/np.linalg.norm(vec1), vec2/np.linalg.norm(vec2)))
     return None
 
 def selectedKeyWords(content):
@@ -85,3 +96,5 @@ def googleSearch(url):
         results = search(keyword, stop = 5)
         for result in results:
             r = requests.post("http://127.0.0.1:8091/visit", data={'url': result})
+
+m = gensim.models.Doc2Vec.load("doc2vec.bin")
